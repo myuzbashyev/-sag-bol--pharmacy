dac488f1d93042100df9de734d2ee4bd809f3476
spacetechnical <megamaster8856@gmail.com>
2024-01-05 19:23:36 +0500

    shoppingCart

--- components/shoppingItems.vue
+++ components/shoppingItems.vue
@@ -1,25 +1,37 @@
 <template>
- <UiShoppingItem
-  class="list-none"
-  v-for="shItem in shItems"
-  :key="shItem?.id"
-  :image="shItem?.img"
-  :title="shItem?.title"
-  :label="shItem?.label"
-  :price="shItem?.price"
-  :pharmacy="shItem?.pharmacy"
-  @delete-item="deleteItem(shItem.id)"
- ></UiShoppingItem>
+  <UiShoppingItem
+    class="list-none"
+    v-for="shItem in shoppingCart"
+    :key="shItem?.id"
+    :image="shItem?.img"
+    :title="shItem?.title"
+    :label="shItem?.label"
+    :price="shItem?.price"
+    :pharmacy="shItem?.pharmacy"
+    @delete-item="deleteItem(shItem.id)"
+    @increase-number="increaseNumber($event, shItem.price, shItem.id)"
+    @decrease-number="decreaseNumber($event, shItem.price, shItem.id)"
+  ></UiShoppingItem>
 </template>
 
 <script>
 export default {
- emits: ["deleteItem"],
- props: ["shItems"],
- methods: {
-  deleteItem(id) {
-   this.$emit("deleteItem", id);
+  data() {
+    return {
+      shoppingCart: useShoppingCart(),
+      item: "",
+    };
   },
- },
+  methods: {
+    deleteItem(id) {
+      this.shoppingCart = this.shoppingCart.filter((item) => item.id !== id);
+    },
+    increaseNumber(event, price, id) {
+      this.shoppingCart.find((item) => item.id === id).price = price * event;
+    },
+    decreaseNumber(event, price, id) {
+      this.shoppingCart.find((item) => item.id === id).price = price * event;
+    },
+  },
 };
 </script>
--- components/ui/shoppingItem.vue
+++ components/ui/shoppingItem.vue
@@ -1,38 +1,55 @@
 <template>
- <li class="border-b">
-  <div class="flex justify-between relative p-10">
-   <div class="flex gap-10">
-    <img :src="image" alt="Medicine image" />
-    <div>
-     <h1 class="font-bold text-xl">{{ label }}</h1>
-     <p class="text-sm">{{ title }}</p>
+  <li class="border-b">
+    <div class="flex justify-between relative p-10">
+      <div class="flex gap-10">
+        <img :src="image" alt="Medicine image" />
+        <div>
+          <h1 class="font-bold text-xl">{{ label }}</h1>
+          <p class="text-sm">{{ title }}</p>
+        </div>
+      </div>
+      <div class="flex gap-10 items-center">
+        <div class="bg-lint py-2 px-5 rounded-lg">
+          <p>{{ pharmacy }}</p>
+        </div>
+        <div class="text-center">
+          <h2 class="font-bold text-lg">{{ price }} TMT</h2>
+          <input
+            type="number"
+            v-model="inputNumber"
+            @mouseup="increaseNumber(inputNumber)"
+            @mousedown="
+              decreaseNumber(inputNumber === 1 ? inputNumber : inputNumber - 1)
+            "
+            min="1"
+            class="text-center bg-lint rounded outline-none"
+          />
+        </div>
+      </div>
+      <i class="pi pi-trash absolute right-3 top-3" @click="deleteItem"></i>
     </div>
-   </div>
-   <div class="flex gap-10 items-center">
-    <div class="bg-lint py-2 px-5 rounded-lg">
-     <p>{{ pharmacy }}</p>
-    </div>
-    <div class="text-center">
-     <h2 class="font-bold text-lg">{{ price }} TMT</h2>
-     <input
-      type="number"
-      value="1"
-      class="text-center bg-lint rounded outline-none"
-     />
-    </div>
-   </div>
-   <i class="pi pi-trash absolute right-3 top-3" @click="deleteItem"></i>
-  </div>
- </li>
+  </li>
 </template>
 <script>
 export default {
- emits: ["deleteItem"],
- props: ["image", "title", "label", "price", "pharmacy"],
- methods: {
-  deleteItem() {
-   this.$emit("deleteItem");
+  emits: ["deleteItem", "increaseNumber", "decreaseNumber"],
+  props: ["image", "title", "label", "price", "pharmacy"],
+  data() {
+    return {
+      inputNumber: 1,
+    };
   },
- },
+
+  methods: {
+    deleteItem() {
+      this.$emit("deleteItem");
+    },
+    increaseNumber(incNumber) {
+      this.$emit("increaseNumber", incNumber);
+    },
+    decreaseNumber(decNumber) {
+      this.$emit("decreaseNumber", decNumber);
+    },
+  },
 };
 </script>
